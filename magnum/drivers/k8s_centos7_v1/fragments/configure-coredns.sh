67f7f35854694f7c217020ebb62e3fd6585f1d86
bbrfkr <1.boogie.5.sophia.4.pop.0@gmail.com>
2019-01-14 00:17:49 +0900

    mod coredns

--- magnum/drivers/k8s_centos7_v1/fragments/configure-coredns.sh
+++ magnum/drivers/k8s_centos7_v1/fragments/configure-coredns.sh
@@ -10,4 +10,5 @@ done
 
 wget -O /tmp/coredns.yaml https://storage.googleapis.com/kubernetes-the-hard-way/coredns.yaml
 sed -i "s/10.32.0.10/${DNS_SERVICE_IP}/g" /tmp/coredns.yaml
+sed -i "s@coredns/coredns:.*@coredns/coredns:1.3.1@g" /tmp/coredns.yaml
 kubectl apply -f /tmp/coredns.yaml
