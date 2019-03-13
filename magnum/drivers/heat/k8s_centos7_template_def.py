d22a3c4cb08d884a1b057fbf6b2154e9e70cbf81
bbrfkr <1.boogie.5.sophia.4.pop.0@gmail.com>
2019-03-13 15:21:19 +0900

    add cluster domain

--- magnum/drivers/heat/k8s_centos7_template_def.py
+++ magnum/drivers/heat/k8s_centos7_template_def.py
@@ -133,6 +133,10 @@ class K8sCentos7TemplateDefinition(k8s_template_def.K8sTemplateDefinition):
                 ca_cert.get_private_key(),
                 ca_cert.get_private_key_passphrase()).replace("\n", "\\n")
 
+        dns_cluster_domain = cluster.labels.get('service_cluster_domain')
+        if dns_cluster_domain:
+            extra_params['dns_cluster_comain'] = dns_cluster_domain
+
         return super(K8sCentos7TemplateDefinition,
                      self).get_params(context, cluster_template, cluster,
                                       extra_params=extra_params,
