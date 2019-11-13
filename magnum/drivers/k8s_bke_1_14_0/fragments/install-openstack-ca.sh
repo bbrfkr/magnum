5b0b7fa3365b04b9438ceb56a39d658b4b982c66
tatsunori.saito <tatsunori.saito@iridge.jp>
2019-11-13 21:35:12 +0900

    mod bug

--- magnum/drivers/k8s_bke_1_14_0/fragments/install-openstack-ca.sh
+++ magnum/drivers/k8s_bke_1_14_0/fragments/install-openstack-ca.sh
@@ -8,6 +8,7 @@ $OPENSTACK_CA
 EOF
     cat >> /opt/stack/venvs/os-collect-config/lib/python2.7/site-packages/requests/cacert.pem <<EOF
 $OPENSTACK_CA
+EOF
     chmod 444 $CA_FILE
     chown root:root $CA_FILE
     update-ca-trust extract
