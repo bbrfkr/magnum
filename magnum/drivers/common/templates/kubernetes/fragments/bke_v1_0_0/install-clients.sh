9f5874969bf981bf070fcc8ca11066602f79f37e
bbrfkr <bbrfkr@gmail.com>
2023-04-15 00:54:01 +0000

    change install path for kubectl

--- magnum/drivers/common/templates/kubernetes/fragments/bke_v1_0_0/install-clients.sh
+++ magnum/drivers/common/templates/kubernetes/fragments/bke_v1_0_0/install-clients.sh
@@ -6,14 +6,11 @@ set +x
 . /etc/sysconfig/heat-params
 set -x
 
-mkdir -p /srv/magnum/bin/
+mkdir -p /usr/local/bin/
 
-curl -o /srv/magnum/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/${KUBE_TAG}/bin/linux/amd64/kubectl
-chmod +x /srv/magnum/bin/kubectl
+curl -o /usr/local/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/${KUBE_TAG}/bin/linux/amd64/kubectl
+chmod +x /usr/local/bin/kubectl
 
 echo "INFO Installed kubectl."
-
-echo "export PATH=/srv/magnum/bin:\$PATH" >> /etc/bashrc
-export PATH=/srv/magnum/bin:$PATH
 
 printf "Finished running ${step}\n"
--- magnum/drivers/common/templates/kubernetes/fragments/install-clients.sh
+++ magnum/drivers/common/templates/kubernetes/fragments/install-clients.sh
@@ -6,14 +6,10 @@ set +x
 . /etc/sysconfig/heat-params
 set -x
 
-mkdir -p /srv/magnum/bin/
 
-curl -o /srv/magnum/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/${KUBE_TAG}/bin/linux/amd64/kubectl
-chmod +x /srv/magnum/bin/kubectl
+curl -o /usr/local/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/${KUBE_TAG}/bin/linux/amd64/kubectl
+chmod +x /usr/local/bin/kubectl
 
 echo "INFO Installed kubectl."
-
-echo "export PATH=/srv/magnum/bin:\$PATH" >> /etc/bashrc
-export PATH=/srv/magnum/bin:$PATH
 
 printf "Finished running ${step}\n"
