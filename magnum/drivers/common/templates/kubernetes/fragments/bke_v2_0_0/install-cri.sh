464d40f6093c802f85682f2e6ff272922bd2b74e
bbrfkr <bbrfkr@gmail.com>
2023-12-28 20:57:33 +0900

    mod to use yq

--- magnum/drivers/common/templates/kubernetes/fragments/bke_v2_0_0/install-cri.sh
+++ magnum/drivers/common/templates/kubernetes/fragments/bke_v2_0_0/install-cri.sh
@@ -5,12 +5,15 @@ echo "START: install cri"
 . /etc/sysconfig/heat-params
 set -x
 
+# initialize containerd config
+containerd config default > /etc/containerd/config.toml
+
 # setting containerd
 if [ -n "${DOCKERHUB_PROXY_URL}" ] ; then
   cat /etc/containerd/config.toml | \
-  yj -tj | \
-  jq ".plugins.\"io.containerd.grpc.v1.cri\".registry.mirrors.\"docker.io\".endpoint |= .+ [\"$DOCKERHUB_PROXY_URL\"]" | \
-  yj -jt > /tmp/config.toml
+  yj -ty | \
+  yq ".plugins.\"io.containerd.grpc.v1.cri\".registry.mirrors.\"docker.io\".endpoint |= .+ [\"$DOCKERHUB_PROXY_URL\"]" | \
+  yj -yt > /tmp/config.toml
   mv /tmp/config.toml /etc/containerd/config.toml
 fi
 
